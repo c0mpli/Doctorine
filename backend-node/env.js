188c7f98f5cbeba97ad1550dc4d0dee467d59bc8
compli <shirothegamer69@gmail.com>
2023-07-01 15:43:53 +0530

    react init

--- backend-node/env.js
+++ backend-node/env.js
@@ -3,4 +3,5 @@ module.exports = {
     "mongodb+srv://root:root@cluster0.kvrgdpp.mongodb.net/?retryWrites=true&w=majority",
   PORT: 5000,
   jwtSecret: "ZoroIsTheBestSwordsman",
+  FLASK_URI: "",
 };
