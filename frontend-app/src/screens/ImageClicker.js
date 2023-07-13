fc78c5f282610c48be289451f54ec3fc7f8151f6
compli <shirothegamer69@gmail.com>
2023-07-13 14:21:49 +0530

    basic setup added

--- frontend-app/src/screens/ImageClicker.js
+++ frontend-app/src/screens/ImageClicker.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Camera } from "expo-camera";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 function ImageClicker() {
   const navigation = useNavigation();
   let cameraRef = useRef(null);
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [photo, setPhoto] = useState(null);
   useEffect(() => {
     (async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
@@ -21,6 +22,51 @@ function ImageClicker() {
       <Text>
         Permission for camera denied. Please change it in the settings.
       </Text>
+    );
+  }
+
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false,
+    };
+
+    let newPhoto = await cameraRef.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
+
+  if (photo) {
+    let savePhoto = async () => {};
+    return (
+      <View style={{ flex: 1, paddingVertical: 120, backgroundColor: "black" }}>
+        <Image
+          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
+          style={{ flex: 1 }}
+        />
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            bottom: 60,
+            left: 40,
+          }}
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <Text style={{ color: "white" }}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            bottom: 60,
+            alignSelf: "center",
+            color: "white",
+          }}
+        >
+          <Text style={{ color: "white" }}>Save</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 
@@ -56,10 +102,7 @@ function ImageClicker() {
           backgroundColor: "white",
         }}
         onPress={async () => {
-          if (cameraRef) {
-            let photo = await cameraRef.takePictureAsync();
-            console.log(photo);
-          }
+          takePic();
         }}
       ></TouchableOpacity>
     </View>
